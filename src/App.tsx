edf707a38fae663558857d5774137aabe8fd3cd4
andresdiazds3 <andresjimenezdiaz123@gmail.com>
2026-02-19 08:55:54 -0500

    Implementacion de Loader con libreria Framer Motion e integracion en App usando useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import ContactCardSection from './components/ContactSection'
+import Loader from './components/Loader'
+import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
 
   return (
     <>
-        <ContactCardSection/>
+        {loading ? <Loader /> : <ContactCardSection />}
     </>
   )
 }
