899ba6795376d91df22b24ebd426326ddcc08fa5
andresdiazds3 <andresjimenezdiaz123@gmail.com>
2026-04-09 16:37:21 -0500

    Feature: Creación de la logica de los distintos tipos de ordenamiento en la clase BinaryTree

--- src/classes/BinaryTree.ts
+++ src/classes/BinaryTree.ts
@@ -35,4 +35,29 @@ export class ArbolBinario {
             }
         }
     }
+
+
+    preorden(node: Node | null = this.raiz, resultado: NodeValue[] = []): NodeValue[] {
+        if (!node) return resultado;
+        resultado.push(node.valor);
+        this.preorden(node.izquierda, resultado);
+        this.preorden(node.derecha, resultado);
+        return resultado;
+    }
+
+    inorder(node: Node | null = this.raiz, resultado: NodeValue[] = []): NodeValue[] {
+        if (!node) return resultado;
+        this.inorder(node.izquierda, resultado);
+        resultado.push(node.valor);
+        this.inorder(node.derecha, resultado);
+        return resultado;
+    }
+
+    postorder(node: Node | null = this.raiz, resultado: NodeValue[] = []): NodeValue[] {
+        if (!node) return resultado;
+        this.postorder(node.izquierda, resultado);
+        this.postorder(node.derecha, resultado);
+        resultado.push(node.valor);
+        return resultado;
+    }
 }
